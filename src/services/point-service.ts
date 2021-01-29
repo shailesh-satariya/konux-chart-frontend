400be76294cc1a81847281f8a7fc2d91652cef40
shailesh-satariya <shailesh.satariya@gmail.com>
2021-01-29 22:49:54 +0530

    The addPoint api request has been corrected and env files have added.

--- src/services/point-service.ts
+++ src/services/point-service.ts
@@ -2,13 +2,17 @@ import axios, {AxiosResponse} from "axios";
 import {Point} from "../types";
 
 const apiUrl: string = process.env.REACT_APP_API_URL as string;
+const headers: Record<string, string> = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+};
 
 export const fetchPoints = (): Promise<AxiosResponse> => {
     return axios.get(`${apiUrl}/data`);
 };
 
 export const addPoint = (point: Point): Promise<AxiosResponse> => {
-    return axios.post(`${apiUrl}/point`, {
+    return axios.post(`${apiUrl}/points`, {
         data: point,
-    });
+    }, {headers});
 };
