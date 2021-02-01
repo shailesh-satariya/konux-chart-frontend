f60126f36354abc30e7cf72a1c84cba9eee14c42
shailesh-satariya <shailesh.satariya@gmail.com>
2021-02-01 16:43:51 +0530

    Config file has been added

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+const CONFIG: Record<string, any> = {
+    dateTimeFormat: 'YYYY-MM-DDTHH:mm:ss.SSS[Z]',
+    timeFormat: 'HH:mm:ss.SSS',
+
+    margins: {top: 50, right: 100, bottom: 50, left: 100},
+    width: 1200,
+    height: 600,
+}
+
+export default CONFIG;
